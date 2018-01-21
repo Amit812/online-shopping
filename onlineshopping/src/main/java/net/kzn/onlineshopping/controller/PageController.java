a144f468c1a002fdf2c4999a9fdb75fd64f095a1
Amit812 <patelgithub812@gmail.com>
2018-01-21 18:07:09 +0530

    Completed the interface theme and other issues

--- onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class PageController {
 	
-	@RequestMapping(value = {"/","/home","/index"})
+	/*@RequestMapping(value = {"/","/home","/index"})
 	public ModelAndView index(){
 		
 		ModelAndView mv = new ModelAndView("page");
@@ -16,7 +16,7 @@ public class PageController {
 		return mv;
 		
 	}
-	
+	*/
 	
 	/*@RequestMapping(value="/test")
 	//public ModelAndView UrlValueTest(@RequestParam("greeting") String greeting){
@@ -51,8 +51,34 @@ public class PageController {
 	}
 	*/
 	
+	@RequestMapping(value = {"/","/home","/index"})
+	public ModelAndView index(){
+		
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "Home");	
+		mv.addObject("userClickHome",true);
+		return mv;
+		
+	}
 	
+	@RequestMapping(value = "/about")
+	public ModelAndView about(){
+		
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "About Us");	
+		mv.addObject("userClickAbout",true);
+		return mv;
+		
+	}
 	
-	
+	@RequestMapping(value = "/contact")
+	public ModelAndView contact(){
+		
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "Contact Us");	
+		mv.addObject("userClickContact",true);
+		return mv;
+		
+	}
 	
 }
